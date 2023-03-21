2ce155f8c04eb7042b63cdd9f74b258e017ab9a0
D3stinn3 <paynedestinne@gmail.com>
2023-03-21 11:32:16 +0300

    Checker

---
+++ checker.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+from http.client import HTTPConnection
+from urllib.parse import urlparse
+
+def site_iko_online(url, timeout=2):
+    """Returns 'True' if the target endpoint is online.
+    Raise an exception otherwise"""
+    error = Exception("Error")
+    parser = urlparse(url)
+    host = parser.netloc or parser.path.split("/")[0]
+    for port in (80, 443, 8000):
+        connection = HTTPConnection(host=host, port=port, timeout=timeout)
+        try:
+            connection.request("HEAD", "/")
+            return True
+        except Exception as e:
+            error = e
+        finally:
+            connection.close()
+    raise error
+
+            

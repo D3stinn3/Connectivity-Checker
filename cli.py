f03687f52d11e4ce42f96ffb247076cb225d3bbb
D3stinn3 <paynedestinne@gmail.com>
2023-03-21 11:58:31 +0300

    Cli Changes

--- cli.py
+++ cli.py
@@ -26,3 +26,11 @@ def read_user_cli_args():
         help="File URLs parser",
     )
     return parser.parse_args()
+
+def display_check_result(result, url, error=""):
+    """Display the result of a connectivity check"""
+    print(f"Status ya URL: '{url}' ni: ", end="")
+    if result:
+        return '"Ipo Site"'
+    return "'Haipo Site'"
+    

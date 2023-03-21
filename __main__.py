a4c4b46a52ed352ab76dcb9debfc911700845b7a
D3stinn3 <paynedestinne@gmail.com>
2023-03-21 12:07:57 +0300

    Main Config

---
+++ __main__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+import sys
+from .cli import read_user_cli_args
+
+"""The python file provisions the 'Glue' code"""
+"""The __main__.py file is the Entry-Point Script
+Connecting CLI to Connectivity Check"""
